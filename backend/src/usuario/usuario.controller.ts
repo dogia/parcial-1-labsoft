510680b588a54a4f1817de6445054e209172dbcf
Daniel Osorio O <dogia@192.168.1.4>
2026-05-14 20:46:29 -0500

    feat(HU04): modulo de usuarios con service, controller y DTOs

---
+++ backend/src/usuario/usuario.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { UsuarioService, UsuarioSinPassword } from './usuario.service';
+import { CrearUsuarioDto } from './dto/crear-usuario.dto';
+import { ActualizarUsuarioDto } from './dto/actualizar-usuario.dto';
+
+@Controller('usuarios')
+export class UsuarioController {
+  constructor(private readonly usuarioService: UsuarioService) {}
+
+  @Get()
+  findAll(): Promise<UsuarioSinPassword[]> {
+    return this.usuarioService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<UsuarioSinPassword> {
+    return this.usuarioService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() dto: CrearUsuarioDto): Promise<UsuarioSinPassword> {
+    return this.usuarioService.create(dto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() dto: ActualizarUsuarioDto): Promise<UsuarioSinPassword> {
+    return this.usuarioService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<void> {
+    return this.usuarioService.remove(id);
+  }
+}
